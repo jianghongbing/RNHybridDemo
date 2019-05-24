7b190d3f11da6246ccedcbd5d2230d4ad40d29ca
jianghongbing <jianghongbing1989@qq.com>
2019-05-24 17:09:43 +0800

    update RNHybridDemo02

---
+++ RNHybridDemo02/ios/RNUserDefaults.h
@@ -0,0 +1,17 @@
+//
+//  RNUserDefaults.h
+//  RNHybridDemo02
+//
+//  Created by pantosoft on 2019/5/24.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNUserDefaults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RNHybridDemo02/ios/RNUserDefaults.m
@@ -0,0 +1,13 @@
+//
+//  RNUserDefaults.m
+//  RNHybridDemo02
+//
+//  Created by pantosoft on 2019/5/24.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "RNUserDefaults.h"
+
+@implementation RNUserDefaults
+
+@end
